109ccc44b1f787ecbfac8cb4af1a6cc4a9dc5bbc
Nikhil <nikhilsai9351@gmail.com>
2026-09-15 14:48:16 +0530

    Update History page

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -55,10 +55,10 @@ export default function History() {
             <thead>
               <tr>
                 <th>#</th>
-                <th>Expression</th>
-                <th>Postfix Form</th>
-                <th>Result</th>
-                <th>Timestamp</th>
+                <th style="color: red;">Expression</th>
+                <th style="color: red;">Postfix Form</th>
+                <th style="color: red;">Result</th>
+                <th style="color: red;" >Timestamp</th>
               </tr>
             </thead>
             <tbody>
